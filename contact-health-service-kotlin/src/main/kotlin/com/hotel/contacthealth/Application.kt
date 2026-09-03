f5e02bf3aed17beb11303607011f1bdb1e745fea
Gabriel Sampaio <gabrielsampaio@google.com>
2026-09-03 18:28:06 +0000

    test(contact-health): adiciona suíte de testes unitários para validação de endpoints

--- contact-health-service-kotlin/src/main/kotlin/com/hotel/contacthealth/Application.kt
+++ contact-health-service-kotlin/src/main/kotlin/com/hotel/contacthealth/Application.kt
@@ -8,18 +8,18 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main() {
-    println("Iniciando Contact Health Service em http://127.0.0.1:8103 ...")
+    embeddedServer(Netty, port = 8103, host = "127.0.0.1", module = Application::module)
+        .start(wait = true)
+}
 
-    embeddedServer(Netty, port = 8103, host = "127.0.0.1") {
-        routing {
-            // Endpoint de Healthcheck (DoD Item 1)
-            get("/healthz") {
-                call.respondText(
-                    text = """{"status":"UP"}""",
-                    contentType = ContentType.Application.Json,
-                    status = HttpStatusCode.OK
-                )
-            }
+fun Application.module() {
+    routing {
+        get("/healthz") {
+            call.respondText(
+                text = """{"status":"UP"}""",
+                contentType = ContentType.Application.Json,
+                status = HttpStatusCode.OK
+            )
         }
-    }.start(wait = true)
+    }
 }
