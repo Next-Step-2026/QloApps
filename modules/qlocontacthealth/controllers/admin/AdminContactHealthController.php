1a23986eaf07b7ec7647a09d39e0f1149f99d84a
Gabriel de Jesus <gabrielsampaio@google.com>
2026-09-11 21:04:20 +0000

    feat(qlocontacthealth): add customer listing and navigation to admin controller
    - Configure customer list table fields (id, firstname, lastname, email, date_upd)
    
    - Handle viewcustomer submit in initContent for profile details
    
    - Add back button in renderView to return to customer list

--- modules/qlocontacthealth/controllers/admin/AdminContactHealthController.php
+++ modules/qlocontacthealth/controllers/admin/AdminContactHealthController.php
@@ -19,7 +19,56 @@ class AdminContactHealthController extends ModuleAdminController
     public function __construct()
     {
         $this->bootstrap = true;
+        $this->table = 'customer';
+        $this->className = 'Customer';
+        $this->lang = false;
+        $this->deleted = false;
+        $this->identifier = 'id_customer';
+        $this->_defaultOrderBy = 'id_customer';
+        $this->_defaultOrderWay = 'DESC';
+        $this->list_no_link = true;
+
         parent::__construct();
+
+        $this->fields_list = array(
+            'id_customer' => array(
+                'title' => $this->l('ID'),
+                'align' => 'text-center',
+                'class' => 'fixed-width-xs',
+            ),
+            'firstname' => array(
+                'title' => $this->l('Nome'),
+                'filter_key' => 'a!firstname',
+            ),
+            'lastname' => array(
+                'title' => $this->l('Sobrenome'),
+                'filter_key' => 'a!lastname',
+            ),
+            'email' => array(
+                'title' => $this->l('E-mail'),
+                'filter_key' => 'a!email',
+            ),
+            'date_upd' => array(
+                'title' => $this->l('Última Atualização'),
+                'type' => 'datetime',
+                'filter_key' => 'a!date_upd',
+            ),
+        );
+
+        $this->addRowAction('view');
+    }
+
+    /**
+     * Initializes controller content based on view display or customer list.
+     */
+    public function initContent()
+    {
+        $customerId = (int) Tools::getValue('id_customer');
+        if ($customerId || Tools::isSubmit('viewcustomer')) {
+            $this->display = 'view';
+        }
+
+        parent::initContent();
     }
 
     /**
@@ -35,6 +84,12 @@ class AdminContactHealthController extends ModuleAdminController
             return parent::renderView();
         }
 
-        return $this->module->renderContactHealthCard($customerId);
+        $backUrl = $this->context->link->getAdminLink('AdminContactHealth');
+        $backButton = '<div class="panel-footer" style="margin-top: 15px;">'
+            . '<a href="' . htmlspecialchars($backUrl, ENT_QUOTES, 'UTF-8') . '" class="btn btn-default">'
+            . '<i class="process-icon-back"></i> ' . $this->l('Voltar para a Lista de Clientes')
+            . '</a></div>';
+
+        return $this->module->renderContactHealthCard($customerId) . $backButton;
     }
 }
